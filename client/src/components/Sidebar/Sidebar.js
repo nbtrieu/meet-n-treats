94463446bc9a1abf2b9cccdd86d7a6be4222edb4
Nicole T <nbtrieu@uci.edu>
2023-02-01 14:18:09 -0800

    Add Logout button to Sidebar.js in Homepage

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,8 +1,20 @@
+import { Link } from 'react-router-dom';
+
+import Auth from '../../utils/auth';
+
 function Sidebar(props) {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
   return (
     <aside className="flex-column space-between py-1">
       <h1>Meet & Treats</h1>
       {props.children}
+      <button className="btn btn-lg btn-light m-2" onClick={logout}>
+        Log out
+      </button>
     </aside>
   )
 };
