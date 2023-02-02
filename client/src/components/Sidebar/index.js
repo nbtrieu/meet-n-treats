e85dcd3e075a17a8c510224a0fbd86264a34b505
justinhwbhang <117044443+justinhwbhang@users.noreply.github.com>
2023-02-02 10:43:02 -0800

    Update index.js

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -1,1 +1,10 @@
-export { default } from "./Sidebar.js";
+function Sidebar(props) {
+  return (
+    <aside className="flex-column space-between py-1">
+      <h1>Meet & Treats</h1>
+      {props.children}
+    </aside>
+  )
+};
+
+export default Sidebar;
