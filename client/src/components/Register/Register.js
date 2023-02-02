e887a248c6eb4587df1763fac6850d779287f51f
justinhwbhang <117044443+justinhwbhang@users.noreply.github.com>
2023-02-02 10:42:08 -0800

    Update Register.js

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -3,7 +3,7 @@ import { useMutation } from "@apollo/client";
 import { REGISTER_USER } from "../../utils/mutations";
 
 import Auth from "../../utils/auth.js";
-
+import image from '../../assets/background.png';
 function Register() {
   const [formState, setFormState] = useState({
     name: "",
@@ -31,33 +31,46 @@ function Register() {
     }
   };
   return (
-    <div>
-      <h1>Register Page</h1>
+    <div className="app-register_page">
+    <div className='app-landing_page__container__header-title'>
+          <h1>PAWSWIPE</h1>
+      </div>
+      <hr className="lin"></hr>
+  <div className="app-login_page__container" style={{backgroundImage: `url(${image})`,
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      height: "70vh",
+      width: "98vw",
+      opacity: "0.9",
+
+}}> 
+       <h1>Sign Up</h1>
       <form onSubmit={handleFormSubmit}>
+      <label htmlFor="email">Username</label>
         <input
           type="text"
-          placeholder="Your username"
           name="name"
           value={formState.name}
           onChange={handleInputChange}
         />
-        <input
-          type="email"
-          placeholder="Your email"
-          name="email"
-          value={formState.email}
-          onChange={handleInputChange}
-        />
+           <label htmlFor="password">Password</label>
         <input
           type="password"
-          placeholder="******"
           name="password"
           value={formState.password}
           onChange={handleInputChange}
         />
-        <button type="submit">Sign up</button>
+        <button type="submit">Register User</button>
       </form>
-    </div>
+
+
+    {/* <div className="app-login_page__container__header">
+      </div> */}
+      </div>
+
+
+  </div>
+
   );
 }
 
