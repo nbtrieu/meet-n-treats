57e43729451c690c9a8c2b79298eb0b854d14a70
hynookim98 <hynookim98@gmail.com>
2023-02-06 04:43:29 -0800

    turned link buy and sell into buttons

--- client/src/pages/Market/MarketHome.js
+++ client/src/pages/Market/MarketHome.js
@@ -13,11 +13,19 @@ function MarketHome() {
                 style={{ textDecoration: 'none' }}>
                 <p className='light-text'>Sell</p>
               </Link>
-  const greeting = `Hello would you like to ${buyLink} or ${sellLink}`;
   return(
-    <div className="page">
-      Hello would you like to {buyLink} or {sellLink} ?
-    </div>)
+    <div className="market-home">
+      <h1 className="market-home__greeting">Welcome to the Marketplace</h1>
+      <div className="market-home__options">
+        <Link to={`/marketplace/buy`} className="market-home__option market-home__option--buy">
+          Buy
+        </Link>
+        <Link to={`/marketplace/sell`} className="market-home__option market-home__option--sell">
+          Sell
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default MarketHome;
