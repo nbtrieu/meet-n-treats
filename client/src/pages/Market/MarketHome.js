b9f4c673aeddcffd969651d7cd03ed5906b97dc7
Nicole T <nbtrieu@uci.edu>
2023-02-06 10:57:37 -0800

    Style Marketplace

--- client/src/pages/Market/MarketHome.js
+++ client/src/pages/Market/MarketHome.js
@@ -14,14 +14,14 @@ function MarketHome() {
                 <p className='light-text'>Sell</p>
               </Link>
   return(
-    <div className="market-home page">
+    <div className="market-home page pet-info-card">
       <h1 className="market-home__greeting">Welcome to the Marketplace</h1>
       <div className="market-home__options">
         <Link to={`/marketplace/buy`} className="market-home__option market-home__option--buy">
-          Buy
+          <h2>Buy</h2>
         </Link>
         <Link to={`/marketplace/sell`} className="market-home__option market-home__option--sell">
-          Sell
+          <h2>Sell</h2>
         </Link>
       </div>
     </div>
