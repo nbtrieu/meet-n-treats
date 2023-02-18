209e0a3be33299213d80bd597312bb8bc8b4f155
Nicole T <nbtrieu@uci.edu>
2023-02-17 21:03:15 -0800

    Restyle search page and add edit buttons to profiles

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -1,31 +1,35 @@
 import React, {useState} from "react";
 import { useQuery } from "@apollo/client";
-import Login from "../../components/Login";
 
 function SearchPage() {
     const [searchTerm, setSearchTerm] = useState("");
 
-    const handleSubmit = (event) => {
+    const handleKeyDown = (event) => {
         event.preventDefault();
         // TODO: Make API call to search for pets or users with the search term
     };
 
     return (
-        <div className="page">
-            <h1>Search</h1>
-            <form onSubmit={handleSubmit}>
+        <div className="my-5 mx-10 flex-column align-start">
+          <div className="row">
+            <div className="col-1 pt-3">
+              <h2>🔎</h2>
+            </div>
+            <div className="col-1 search-box">
+              <form>
                 <input 
-                    type="text"
-                    placeholder="Search by pet name or username"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
+                  type="text"
+                  placeholder="Search by pet name or username"
+                  value={searchTerm}
+                  tabIndex="-1"
+                  onKeyDown={handleKeyDown}
                 />
-                <button type="submit">Search</button>
-            </form>
-            <h2>Results</h2>
-            <ul>
-                {/* TODO: Add display search results */}
-            </ul>
+              </form>
+            </div>
+          </div>
+          <ul>
+              {/* TODO: Add display search results */}
+          </ul>
         </div>
     );
 }
